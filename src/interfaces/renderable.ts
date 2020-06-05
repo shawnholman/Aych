8b295861069a57a19a40be2d235be058d24bb28b
Shawn Holman <the.ikick@gmail.com>
2020-06-05 01:01:53 -0400

    Added comment

--- src/interfaces/renderable.ts
+++ src/interfaces/renderable.ts
@@ -3,7 +3,7 @@ import {SimpleObject} from "./simple-object";
 /** Rendering Operation */
 export interface Renderable {
     /**
-     *
+     * A render HTML string
      * @param templates
      */
     render(templates?: SimpleObject): string;
