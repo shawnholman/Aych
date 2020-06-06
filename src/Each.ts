40ad7755db5aabad2056dc72638400d8ba48ea8e
Shawn Holman <the.ikick@gmail.com>
2020-06-06 12:31:16 -0400

    The Each Renderable will take a Renderable and create copies of it based on a iterable. The value of these iterables will be available to the children renderables.

---
+++ src/Each.ts
@@ -0,0 +1,69 @@
+import {Renderable, SimpleObject} from "./interfaces";
+import {isRenderable, isString} from "./Util";
+import {StringLiteral} from "./StringLiteral";
+
+type EachRenderFunction = (item: string, index: number, items: Iterable<any>) => (Renderable | string);
+
+/** Creates rendered elements based on an Iterable */
+export class Each implements Renderable {
+    private readonly items: Iterable<any>;
+    private readonly element: Renderable;
+    private readonly renderFunction: EachRenderFunction;
+
+    constructor(items: Iterable<any>, element: EachRenderFunction | Renderable | string) {
+        this.items = items;
+
+        if (isString(element)) {
+            this.element = new StringLiteral(element);
+        } else if (isRenderable(element)) {
+            this.element = element;
+        } else {
+            this.renderFunction = element;
+        }
+    }
+
+    /** @inheritdoc */
+    render(templates?: SimpleObject): string {
+        if (this.element === undefined) {
+            return this.renderByFunction(templates);
+        } else {
+            return this.renderByElement(templates);
+        }
+    }
+
+    /**
+     * Renders elements directly using a Renderable
+     * @param templates
+     */
+    private renderByElement(templates?: SimpleObject): string {
+        let render = '';
+        let i = 0;
+        for (let item of this.items) {
+            let iteration = { item, i };
+            render += this.element.render({ ...iteration, ...templates });
+            i++;
+        }
+        return render;
+    }
+
+    /**
+     * Renders elements using a function that generates the element.
+     * @param templates
+     */
+    private renderByFunction(templates?: SimpleObject): string {
+        let render = '';
+        let i = 0;
+        for (let item of this.items) {
+            let element = this.renderFunction(item, i, this.items);
+            let iteration = { item, i };
+
+            if (isString(element)) {
+                element = new StringLiteral(element);
+            }
+
+            render += element.render({ ...iteration, ...templates });
+            i++;
+        }
+        return render;
+    }
+}
---
+++ tests/each.test.ts
@@ -0,0 +1,37 @@
+import { expect } from 'chai';
+import {NestableElement} from "../src/elements";
+import {Each} from "../src/Each";
+
+describe('Each', () => {
+    const TEST_ARRAY = ['dog', 'cat', 'rat'];
+
+    it('renders nothing if the list is empty', () => {
+        const element = new Each([], new NestableElement('div'));
+        const rendered = element.render();
+        expect(rendered).to.equal('');
+    });
+
+    it('renders a list of div\'s utilizing the built in templates', () => {
+        const element = new Each(TEST_ARRAY, new NestableElement('div', '{{i}}:{{item}}'));
+        const rendered = element.render();
+        expect(rendered).to.equal('<div>0:dog</div><div>1:cat</div><div>2:rat</div>');
+    });
+
+    it('renders a list of div\'s utilizing an EachRenderFunction', () => {
+        const element = new Each(TEST_ARRAY, (item, i) => new NestableElement('div', `${i}:${item}`));
+        const rendered = element.render();
+        expect(rendered).to.equal('<div>0:dog</div><div>1:cat</div><div>2:rat</div>');
+    });
+
+    it('renders a list of strings utilizing the built in templates', () => {
+        const element = new Each(TEST_ARRAY,'{{i}}:{{item}};');
+        const rendered = element.render();
+        expect(rendered).to.equal('0:dog;1:cat;2:rat;');
+    });
+
+    it('renders a list of strings utilizing an EachRenderFunction', () => {
+        const element = new Each(TEST_ARRAY, (item, i) => `${i}:${item};`);
+        const rendered = element.render();
+        expect(rendered).to.equal('0:dog;1:cat;2:rat;');
+    });
+});
