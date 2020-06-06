4b4cdb9d8c0a8903989e5af1c7718292ed054b76
Shawn Holman <the.ikick@gmail.com>
2020-06-06 12:35:26 -0400

    Basic pipes added to Aych.

--- src/Aych.ts
+++ src/Aych.ts
@@ -1,5 +1,17 @@
 export class Aych {
 
+    public static Pipes = class {
+        [key: string]: string;
+
+        static uppercase(str: string): string {
+            return str.toUpperCase();
+        }
+
+        static substr(str: string, from: string, length: string): string {
+            console.log(str, from, length);
+            return str.substr(parseInt(from), parseInt(length));
+        }
+    }
 }
 
 export const H = new Aych();
