fe28fa45dbf8e12a46b1d49a46ada21af5b36bb4
Shawn Holman <the.ikick@gmail.com>
2020-06-06 12:33:14 -0400

    Adjusted the type identifiers to accept any object to test. This makes more sense when we have no set type of item to test.

--- src/Util.ts
+++ src/Util.ts
@@ -4,7 +4,7 @@
  */
 import {Attributes, Renderable} from "./interfaces";
 
-export function isRenderable(param?: string | Renderable | Attributes): param is Renderable {
+export function isRenderable(param?: any): param is Renderable {
     return param !== undefined && (param as Renderable).render !== undefined;
 }
 
@@ -12,14 +12,14 @@ export function isRenderable(param?: string | Renderable | Attributes): param is
  * Determines if parameter is a a string
  * @param param
  */
-export function isString(param?: string | Renderable | Attributes): param is string {
+export function isString(param?: any): param is string {
     return param !== undefined && typeof param === 'string';
 }
 
-/**
+/**s
  * Determines if parameter is a Attributes
  * @param param
  */
-export function isAttributes(param?: string | Renderable | Attributes): param is Attributes {
+export function isAttributes(param?: any): param is Attributes {
     return param !== undefined && (!isString(param) && !isRenderable(param));
 }
