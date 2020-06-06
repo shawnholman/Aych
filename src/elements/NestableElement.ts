a046a9e54b7ce1e5b134adaeef042c8a8bbe6e76
Shawn Holman <the.ikick@gmail.com>
2020-06-06 12:32:22 -0400

    JSDoc fix

--- src/elements/NestableElement.ts
+++ src/elements/NestableElement.ts
@@ -2,6 +2,7 @@ import {SimpleObject} from "../interfaces";
 import {RenderableElement} from "./RenderableElement";
 
 export class NestableElement extends RenderableElement {
+    /** @inheritdoc */
     internalRender(templates?: SimpleObject): string {
         const TAG = this.getTag();
         const attributes = this.getHtmlAttributeList();
