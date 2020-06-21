a5d9c4078861e957de8188a7915f949f956a0f59
shawnholman <shawnholman@uga.edu>
2020-06-21 00:48:24 -0400

    Circular dependency detection

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var nodeExternals = require('webpack-node-externals');
+const CircularDependencyPlugin = require('circular-dependency-plugin');
 
 module.exports = {
     mode: "development",
@@ -21,6 +22,21 @@ module.exports = {
             }
         ]
     },
+    plugins: [
+        new CircularDependencyPlugin({
+            // exclude detection of files based on a RegExp
+            exclude: /a\.js|node_modules/,
+            // include specific files based on a RegExp
+            include: /src/,
+            // add errors to webpack instead of warnings
+            failOnError: true,
+            // allow import cycles that include an asyncronous import,
+            // e.g. via import(/* webpackMode: "weak" */ './file.js')
+            allowAsyncCycles: false,
+            // set the current working directory for displaying module paths
+            cwd: process.cwd(),
+        })
+    ],
     resolve: {
         extensions: [ '.ts', '.tsx', '.js' ]
     },
